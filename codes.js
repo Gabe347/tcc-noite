664c87ca2e56e836b14d650de658214dbe75d52a
Gabe347 <repositorialdomygitdesk@proton.me>
2024-08-06 12:55:22 -0300

    a

--- codes.js
+++ codes.js
@@ -29,5 +29,11 @@ window.addEventListener('mouseup', function(event) {
 function telalogin(){
     var btn = document.createElement('img');
     btn.src = "Imagens/userg.png"
+    var perfilmenu = document.querySelector('.perfilm');
+
+    if (perfilmenu.style.display === 'block'){
+        open("https://github.com/Gabe347/tcc-noite")
+    }
+    
 }
 
